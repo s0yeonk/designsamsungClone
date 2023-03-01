001654a227989770c88e7f7bd4bbab455e7647c8
김소연 <tcb05019@naver.com>
2023-03-02 04:06:46 +0900

    visual-s

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 'use strict'
+AOS.init();
 const $searchOpenBtn=document.querySelector('.widget-search');
 const $searchContainer=document.querySelector('.search-container');
 $searchOpenBtn.addEventListener('click',()=>{
