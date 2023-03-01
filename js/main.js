13c367ef5502a6c5e6251ca551407e92400def28
김소연 <tcb05019@naver.com>
2023-03-02 00:55:11 +0900

    css

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ const $searchContainer=document.querySelector('.search-container');
 $searchOpenBtn.addEventListener('click',()=>{
   $searchContainer.classList.add('isActive')
 })
+
+const $mbMenu=document.querySelector('#mb-menu');
+const $mbBtn=document.querySelector('.widget-navbtn');
+$mbBtn.addEventListener('click',()=>{
+  $mbMenu.classList.remove('d-none')
+})
